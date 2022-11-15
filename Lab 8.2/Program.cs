f1c98727541bb4bdf2fbcf5ee5f8e1703811dde9
krist <krist@LAPTOP-HIQLHLL6>
2022-11-15 23:17:04 +0500

    Добавьте файлы проекта.

---
+++ Lab 8.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Temp\Numbers.txt";
+            Random ran = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false)) 
+            {
+                for (int i =0; i<10; i++)
+                {
+                    sw.WriteLine(ran.Next(0,10));
+                }
+            }
+            int s = 0;
+            using (StreamReader sr = new StreamReader(path)) 
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    int k = Convert.ToInt32(sr.ReadLine());
+                    s += k;
+                }
+            }
+            Console.WriteLine( s );
+            Console.ReadKey();
+        }
+    }
+}
